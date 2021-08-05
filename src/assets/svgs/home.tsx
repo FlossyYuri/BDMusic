00dbfdf22f9058291174c99b4886ec16419ce0b6
emerson <ecuambe@technoplus.co.mz>
2021-08-06 00:03:34 +0200

    Add Assets

---
+++ src/assets/svgs/home.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+const HomeIcon = () => (
+    <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M1.74976 7.30012L9.99976 1.00012L18.2498 7.30012V17.2001C18.2498 17.6775 18.0566 18.1353 17.7128 18.4729C17.369 18.8105 16.9027 19.0001 16.4164 19.0001H3.58309C3.09686 19.0001 2.63054 18.8105 2.28673 18.4729C1.94291 18.1353 1.74976 17.6775 1.74976 17.2001V7.30012Z"
+            stroke="#70BCCE"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+        <path
+            d="M7.75024 19.0004V10.0004H12.2502V19.0004"
+            stroke="#70BCCE"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+    </svg>
+)
+export default HomeIcon
